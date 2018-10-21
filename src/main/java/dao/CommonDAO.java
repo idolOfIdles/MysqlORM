69bf4d541656b3bc8ffdd69f64e064d755a52c8a
safayat <safayat@cefalo.com>
2018-10-22 00:40:40 +0600

    jdbc added

--- src/main/java/dao/CommonDAO.java
+++ src/main/java/dao/CommonDAO.java
@@ -1,9 +1,6 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by safayat on 10/20/18.
@@ -74,5 +71,42 @@ public class CommonDAO {
         }
 
     }
+
+    public String getAll(String id)  {
+
+        Connection dbConnection = null;
+        PreparedStatement statement = null;
+        String bank_name = "";
+
+        try {
+            dbConnection = getConnection();
+            statement = dbConnection.prepareStatement("SELECT cl.accountNumber, cl.address, ac.clientId, ac.accountNumber FROM accountInfo ac RIGHT JOIN client cl ON ac.clientId = cl.id");
+            ResultSet rs = statement.executeQuery();
+
+            if(rs.next()){
+                System.out.println(rs.getMetaData());
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            if(statement!=null){
+                try {
+                    statement.close();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if(dbConnection!=null){
+                try {
+                    dbConnection.close();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return bank_name;
+    }
+
 
 }
---
+++ src/main/java/model/AccountInfo.java
@@ -0,0 +1,44 @@
+package model;
+
+/**
+ * Created by safayat on 10/21/18.
+ */
+public class AccountInfo {
+
+    private int id;
+    private int clientId;
+    private String accountNumber;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
